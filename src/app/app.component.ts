46e3d7564bc414b0f00e9f8e161c65ed48e23aaa
Omar Letona <omarletonaf@gmail.com>
2021-02-02 16:59:47 -0600

    updated social buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlatarformService } from './services/platarform.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'repinta-website';
+export class AppComponent implements OnInit{
+  mobile:boolean = true;
+  constructor(private platformService:PlatarformService){}
+  ngOnInit(): void {
+    this.mobile = this.platformService.isMobile();
+  }
+  title = 'Repinta';
+
+
 }
