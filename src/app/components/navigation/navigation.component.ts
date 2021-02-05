236c58a5a68b24818e23cf5387f54c4a2d89d3f5
Omar Letona <omarletonaf@gmail.com>
2021-02-05 09:30:21 -0600

    Changed navbar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  repintaLogo = 'https://firebasestorage.googleapis.com/v0/b/repinta-7d160.appspot.com/o/repinta_nobg_logo.png?alt=media&token=cca0cc3a-6882-4a34-8fdb-a441c995eea1'
+  repintaLogo = 'https://firebasestorage.googleapis.com/v0/b/repinta-7d160.appspot.com/o/repinta_nobg_logo_croped.png?alt=media&token=50948fdc-a5ca-4d6d-afc7-078763abb6fe'
   constructor() { }
 
   ngOnInit(): void {
